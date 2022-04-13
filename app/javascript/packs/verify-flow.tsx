4ebaf097e4069be3d9ae21895bfbf8eca778e312
Andrew Duthie <andrew.duthie@gsa.gov>
2022-04-13 09:21:52 -0400

    Refactor SecretsContextProvider as observable initializer
    - Avoid waiting to render the app
    - Manage subscribers automatically via context value change

--- app/javascript/packs/verify-flow.tsx
+++ app/javascript/packs/verify-flow.tsx
@@ -1,22 +1,14 @@
 import { render } from 'react-dom';
-import { VerifyFlow, SecretValues, SecretsContextProvider } from '@18f/identity-verify-flow';
-import SecretSessionStorage, { encode } from '@18f/identity-secret-session-storage';
+import { VerifyFlow, SecretsContextProvider } from '@18f/identity-verify-flow';
+import { encode } from '@18f/identity-secret-session-storage';
 
 const appRoot = document.getElementById('app-root')!;
-(async () => {
-  const key = encode(atob(appRoot.dataset.storeKey!));
-  const iv = encode(atob(appRoot.dataset.storeIv!));
+const key = encode(atob(appRoot.dataset.storeKey!));
+const iv = encode(atob(appRoot.dataset.storeIv!));
 
-  const storage = new SecretSessionStorage<SecretValues>();
-  storage.storageKey = 'verify';
-  storage.key = await crypto.subtle.importKey('raw', key, 'AES-GCM', true, ['encrypt', 'decrypt']);
-  storage.iv = iv;
-  await storage.load();
-
-  render(
-    <SecretsContextProvider value={storage}>
-      <VerifyFlow />
-    </SecretsContextProvider>,
-    appRoot,
-  );
-})();
+render(
+  <SecretsContextProvider storeKey={key} storeIV={iv}>
+    <VerifyFlow />
+  </SecretsContextProvider>,
+  appRoot,
+);
